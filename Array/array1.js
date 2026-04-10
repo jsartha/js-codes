804e07b39a664e33b1207ebe3d44bd24c97bf12b
jsartha <jsarthak168@gmail.com>
2026-04-10 08:45:42 +0000

    config change

--- Array/array1.js
+++ Array/array1.js
@@ -1,2 +1,55 @@
 let array=[1,2,3,4,5,6,7,89,0]
 console.log(array)
+
+// mthods 
+
+//push 
+
+
+array.push(100)
+console.log(array);
+
+//pop
+array.pop()
+console.log();
+
+
+// unshift()
+
+array.unshift(999)
+console.log(array);
+
+
+//shift()
+
+array.shift()
+console.log(array);
+
+
+//Join()
+
+
+const newArray=array.join()
+
+console.log(newArray);
+
+
+//slice()
+
+array.slice(1,3)
+console.log(array);
+
+//splice()
+
+array.splice(1,3)
+
+console.log(splice);
+
+//imp
+
+
+
+
+
+
+
