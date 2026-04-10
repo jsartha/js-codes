5baf3ee17434beb269c960a003f137bc85c953bd
jsartha <jsarthak168@gmail.com>
2026-04-10 09:30:09 +0000

    config data

---
+++ Array/concat.js
@@ -0,0 +1,18 @@
+const array8=['sarthak' ,'Sakshi' ,'Priti' ,'Pravin','Navneet']
+
+const college=['Pune' , 'diploma', 'Avcoe']
+
+//concat 
+
+//   array8.push(college)
+
+// console.log(array8);
+
+
+// solution concat
+
+console.log('used concat method');
+
+const Alldata=array8.concat(college)
+
+console.log(Alldata);
