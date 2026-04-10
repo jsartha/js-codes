09b9035c136964a125b8b1d0418c8947f9807040
jsartha <jsarthak168@gmail.com>
2026-04-10 09:30:41 +0000

    config data

---
+++ Array/Spread.js
@@ -0,0 +1,10 @@
+
+const a=['sarthak' ,'Sakshi' ,'Priti' ,'Pravin','Navneet']
+
+const college2=['Pune' , 'diploma', 'Avcoe']
+
+const spread=[ ...a , ...college2]
+
+console.log(spread);
+
+//same work for concat
---
+++ Array/flat.js
@@ -0,0 +1,8 @@
+//multiple array can conver in single array
+
+let Array=[1,2,3,4,5,['s','r'],['t','h'],'k']
+
+
+const arraydata=Array.flat(1) //.....> 1 sey depth of  arrya
+
+console.log(arraydata);
