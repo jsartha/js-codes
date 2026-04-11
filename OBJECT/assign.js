61270f11208eb8ed54be59aef6ac9112bb13610d
jsartha <jsarthak168@gmail.com>
2026-04-11 05:59:08 +0000

    done

---
+++ OBJECT/assign.js
@@ -0,0 +1,30 @@
+
+// let data=new Object();
+
+// let data2= new Object();
+
+
+
+// data.name={ 1:"a" ,2:"b"}
+                                            
+// data2.name2={ 1:"A" , 2:"B"}
+
+
+// const assigndata=Object.assign({},data.name,data2.name2)
+
+
+// console.log(assigndata);
+
+
+
+const data={}
+
+const data2={}
+
+
+data.name={ 1:"a" ,2:"b"}
+                                        
+data2.name2={ 1:"A" , 2:"B"}
+
+const resultassign=Object.assign({},data ,data2)
+console.log(resultassign);
