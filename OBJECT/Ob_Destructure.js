3654837ab7bbb4c55e541ebdb8558ce6f60511e1
jsartha <jsarthak168@gmail.com>
2026-04-12 07:04:34 +0000

    config data

---
+++ OBJECT/Ob_Destructure.js
@@ -0,0 +1,26 @@
+
+
+const course={
+
+    course_name:"js in hindi " ,
+    price:"99494" ,
+    courseInstance:"chai or code"
+}
+
+
+
+//course.courseInstance
+
+// const {courseInstance }=course
+
+//console.log( courseInstance);
+
+
+// change name 
+
+
+
+const {courseInstance : instructor}=course
+
+
+console.log(instructor);
