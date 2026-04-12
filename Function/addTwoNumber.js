4bbb5e98418d48269102e1ade4deefceca80e871
jsartha <jsarthak168@gmail.com>
2026-04-12 07:31:35 +0000

    config data

---
+++ Function/addTwoNumber.js
@@ -0,0 +1,53 @@
+
+//Example 1
+function addTwoNumber(){
+
+    return 20+90
+}
+
+console.log(addTwoNumber())
+
+addTwoNumber()
+
+
+//example 2
+
+
+function twonumber(a ,b){   //Parameter mantat  karan definition  apply kart
+
+    console.log(a+b);
+    
+    
+}
+
+
+twonumber(201 ,90) // argument 
+
+
+// Example 3
+
+
+function Two ( a, b){
+
+
+   console.log( a+b);
+    
+}
+
+
+Two(9,8);
+
+
+
+function Two1(a , b){
+
+
+    let result=a + b
+
+    return result
+}
+
+
+console.log(Two1( 30 ,40))
+
+console.log('myresult', result);
