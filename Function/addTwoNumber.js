aaa21157c0408c620233fd62a047ef9293d036a6
jsartha <jsarthak168@gmail.com>
2026-04-13 10:08:37 +0000

    config

--- Function/addTwoNumber.js
+++ Function/addTwoNumber.js
@@ -75,3 +75,20 @@ console.log( loginUser());
 
 
 //Example 5
+
+function loginUser2(username1) {
+
+    if(!username1) {
+
+
+        console.log('plese fill the username');
+     
+        return
+    }
+
+    return  `${username1} , just loggin two`
+    
+}
+
+
+console.log( loginUser2());
