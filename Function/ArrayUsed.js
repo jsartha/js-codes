7efcd0b552ba9461d4a3769b0fe0618ffb371b52
jsartha <jsarthak168@gmail.com>
2026-04-13 11:18:40 +0000

    done

--- Function/ArrayUsed.js
+++ Function/ArrayUsed.js
@@ -4,9 +4,17 @@ const Myarray=[1,2,3,4,56,7,8,9,0]
 
 function handleArray(arrayObject){
 
-    console.log(`  show the my return array ${arrayObject[7]}`);
+    console.log(`  show the my return array ${arrayObject[2]}`);
     
       return
 }
 
 handleArray(Myarray)
+
+// create to arguments
+console.log(
+handleArray(
+    [9389,8899,1233445]
+)
+
+)
