f72b55cf14b7b07d5c161a09014c0529b06876cd
jsartha <jsarthak168@gmail.com>
2026-04-14 07:11:47 +0000

    save

---
+++ Function/ArowFunction/This.js
@@ -0,0 +1,21 @@
+const { use } = require("react");
+
+const user={
+
+    username:"Sakshi" ,
+    price:743983 ,
+
+
+    welcomeMessageFunction: function(){
+
+
+        console.log(`${this.username} welcome to the website`);
+        
+    }
+}
+
+user.welcomeMessageFunction()
+
+user.username="sarthak"
+
+use.welcomeMessageFunction()
