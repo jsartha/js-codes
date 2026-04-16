66ad4427965619f589a644905bf4ed225a3fb971
jsartha <jsarthak168@gmail.com>
2026-04-16 20:16:59 +0000

    config

--- Loops/arrayLoop.js
+++ Loops/arrayLoop.js
@@ -17,6 +17,7 @@ let array1=[1,2,3,4,56,7,8]
 
 for (let i=0 ; i<array1.length; i++){
 
+    
     console.log(array1[i]);
     
 }
---
+++ Loops/do_while.js
@@ -0,0 +1,11 @@
+
+let i=0
+do{
+
+console.log(i);
+
+i=i+2
+
+}
+while (i<=10) 
+    
