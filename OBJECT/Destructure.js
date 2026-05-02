579497f6a10c656a77e9fd90fa8b9d4633231c7e
jsartha <jsarthak168@gmail.com>
2026-05-02 14:50:14 +0000

    config

--- OBJECT/Destructure.js
+++ OBJECT/Destructure.js
@@ -1,12 +1,22 @@
+
+
 const  user={
 
     name:"sarthak" ,
 
     amount:9039,
 
-    emial:"jsarthak@gmail.com"
+    emial:"jsarthak@gmail.com" ,
+
+    greeting: function() {
+        
+        console.log(' hi');
+        
+     }
 
 }
+
+user.greeting()
 
 const { name:username , amount:total_amount}=user
 
@@ -15,6 +25,7 @@ const { name:username , amount:total_amount}=user
 
 
 console.log(username , total_amount);
+
 
 
 
@@ -37,3 +48,85 @@ for( const keys of Object.entries(user) ){
     console.log(keys);
     
 }
+
+
+
+for( const [keys ,values] of Object.entries(user) ){
+
+    console.log(keys , values);
+    
+}
+
+
+
+/// nested loop
+
+
+const  user1={
+
+    name:"sarthak" ,
+
+    amount:9039,
+
+    emial:"jsarthak@gmail.com" ,
+
+    // greeting: function() {
+        
+    //     console.log(' hi');
+        
+    //  }
+        
+
+    user2:{
+
+    name:"sarthak" ,
+
+    amount:9039,
+
+    emial:"jsarthak@gmail.com" 
+
+         ,
+
+        0:"sarthak" ,            /// array create 
+        
+         
+        1:"joshi"
+
+
+}
+}
+
+console.log(user1[0]);
+
+
+
+console.log(user1.user2.amount);
+
+//shallow copy
+//const user11={...user1}
+
+/*
+user11.amount=9900
+
+console.log(user11);
+
+
+console.log(user1);
+
+*/
+//jar apan as kel tar fact parent value var effct nahi honar chiled var hoyil
+
+
+// Tya sathi 
+// DEEp  value consept
+
+
+const user123=structuredClone(user1) ;
+
+user123.user2.emial="urgjg"
+console.log(user123);
+
+console.log(user1);
+
+
+
--- OBJECT/Ob_Destructure.js
+++ OBJECT/Ob_Destructure.js
@@ -1,4 +1,7 @@
+ 
+ 
 
+ 
 
 const course={
 
